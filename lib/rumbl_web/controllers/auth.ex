e11095919115745e2a3830ef1b9d391892b5d003
Dawid Dęby <dawid.deby@gmail.com>
2021-01-26 22:00:06 +0100

    Logout. There is still issue with login when user create new account

--- lib/rumbl_web/controllers/auth.ex
+++ lib/rumbl_web/controllers/auth.ex
@@ -10,6 +10,10 @@ defmodule RumblWeb.Auth do
     |> configure_session(renew: true)
   end
 
+  def logout(conn) do
+    configure_session(conn, drop: true)
+  end
+
   def call(conn, _opts) do
     user_id = get_session(conn, :user_id)
     user = user_id && Rumbl.Accounts.get_user(user_id)
