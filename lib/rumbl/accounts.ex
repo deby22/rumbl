16d7772767c02587a05f438f2e5b71b63830d816
Dawid Dęby <dawid.deby@gmail.com>
2021-01-26 16:58:29 +0100

    Create User form

--- lib/rumbl/accounts.ex
+++ lib/rumbl/accounts.ex
@@ -3,23 +3,34 @@ defmodule Rumbl.Accounts do
   The accounts context.
   """
 
+  alias Rumbl.Repo
   alias Rumbl.Accounts.User
 
+
   def list_users do
-    [
-      %User{id: "1", name: "Dawid", username: "ddeby"},
-      %User{id: "2", name: "Marcin", username: "yetem"},
-      %User{id: "3", name: "Wojtek", username: "drizi"}
-    ]
+    Repo.all(User)
   end
 
   def get_user(id) do
-    Enum.find(list_users(), fn map -> map.id == id end)
+    Repo.get(User, id)
+  end
+
+  # raises an Ecto.NotFoundError when looking up a user that does not exist.
+  def get_user!(id) do
+    Repo.get!(User, id)
   end
 
   def get_user_by(params) do
-    Enum.find(list_users(), fn map ->
-      Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
-    end)
+    Repo.get_by(User, params)
+  end
+
+  def change_user(%User{} = user) do
+    User.changeset(user, %{})
+  end
+
+  def create_user(attrs \\ %{}) do
+    %User{}
+    |> User.changeset(attrs)
+    |> Repo.insert()
   end
 end
