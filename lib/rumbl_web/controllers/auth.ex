b43945f21d291e23afef775ee0b1bf3698defc68
Dawid Dęby <dawid.deby@gmail.com>
2021-01-26 21:47:10 +0100

    Authenticate. Loginin by using session

--- lib/rumbl_web/controllers/auth.ex
+++ lib/rumbl_web/controllers/auth.ex
@@ -3,6 +3,13 @@ defmodule RumblWeb.Auth do
 
   def init(opts), do: opts
 
+  def login(conn, user) do
+    conn
+    |> assign(:current_user, user)
+    |> put_session(:user_id, user.id)
+    |> configure_session(renew: true)
+  end
+
   def call(conn, _opts) do
     user_id = get_session(conn, :user_id)
     user = user_id && Rumbl.Accounts.get_user(user_id)
