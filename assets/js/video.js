e5461a250d66ea2a2237a01a04cc68345497cc65
Dawid Dęby <dawid.deby@gmail.com>
2021-01-28 18:49:22 +0100

    jump to second after click annotation

--- assets/js/video.js
+++ assets/js/video.js
@@ -13,44 +13,76 @@ let Video = {
         })
     },
 
-    onReady(videoId, socket){
+    onReady(videoId, socket) {
         let msgContainer = document.getElementById("msg-container")
         let msgInput = document.getElementById("msg-input")
         let postButton = document.getElementById("msg-submit")
-        postButton.addEventListener("click", e =>{
-            let payload = {body: msgInput.value, at: Player.getCurrentTime()}
+        postButton.addEventListener("click", e => {
+            let payload = { body: msgInput.value, at: Player.getCurrentTime() }
             vidChannel.push("new_annotation", payload)
-                .receive("error", e => console.log(e) )
+                .receive("error", e => console.log(e))
             msgInput.value = ""
         })
 
-        
+
         let vidChannel = socket.channel("videos:" + videoId)
         vidChannel.on("new_annotation", (resp) => {
             this.renderAnnotation(msgContainer, resp)
         })
-        vidChannel.on("ping", ({count}) => console.log("PING", count) )
+        vidChannel.on("ping", ({ count }) => console.log("PING", count))
         vidChannel.join()
-            .receive("ok", resp => console.log("joined to the video channel", resp) )
-            .receive("error", reason => console.log("Join failed", reason) )
-   
+            .receive("ok", resp => {
+                this.scheduleMessages(msgContainer, resp.annotations)
+            })
+            .receive("error", reason => console.log("Join failed", reason))
+
+        msgContainer.addEventListener("click", e => {
+            e.preventDefault()
+            let seconds = e.target.getAttribute("data-seek") ||
+                e.target.parentNode.getAttribute("data-seek")
+            if (!seconds) { return }
+            Player.seekTo(seconds)
+        })
     },
-    
+
     esc(str) {
         let div = document.createElement("div")
         div.appendChild(document.createTextNode(str))
         return div.innerHTML
     },
 
-    renderAnnotation(msgContainer, {user, body, at}) {
+    renderAnnotation(msgContainer, { user, body, at }) {
         let template = document.createElement("div")
         template.innerHTML = `
             <a href="#" data-seek="${this.esc(at)}">
+                [${this.formatTime(at)}]
                 <b>${this.esc(user.username)}</b>: ${this.esc(body)}
             </a>
         `
         msgContainer.appendChild(template)
         msgContainer.scrollTop = msgContainer.scrollHeight
-    } 
+    },
+    scheduleMessages(msgContainer, annotations) {
+        clearTimeout(this.scheduleTimer)
+        this.scheduleTimer = setTimeout(() => {
+            let ctime = Player.getCurrentTime()
+            let remaining = this.renderAtTime(annotations, ctime, msgContainer)
+            this.scheduleMessages(msgContainer, remaining)
+        }, 1000)
+    },
+    renderAtTime(annotations, seconds, msgContainer) {
+        return annotations.filter(ann => {
+            if (ann.at > seconds) { return true }
+            else {
+                this.renderAnnotation(msgContainer, ann)
+                return false
+            }
+        })
+    },
+    formatTime(at) {
+        let date = new Date(null)
+        date.setSeconds(at / 1000)
+        return date.toISOString().substr(14, 5)
+    }
 }
 export default Video
--- lib/rumbl_web/channels/video_channel.ex
+++ lib/rumbl_web/channels/video_channel.ex
@@ -2,9 +2,17 @@ defmodule RumblWeb.VideoChannel do
   use RumblWeb, :channel
   alias Rumbl.Accounts
   alias Rumbl.Multimedia
+  alias RumblWeb.AnnotationView
 
   def join("videos:" <> video_id, _params, socket) do
-    {:ok, assign(socket, :video_id, String.to_integer(video_id))}
+    video_id = String.to_integer(video_id)
+    video = Multimedia.get_video!(video_id)
+    annotations =
+      video
+      |> Multimedia.list_annotations()
+      |> Phoenix.View.render_many(AnnotationView, "annotation.json")
+
+    {:ok, %{annotations: annotations}, assign(socket, :video_id, video_id)}
   end
 
   def handle_in(event, params, socket) do
@@ -12,7 +20,7 @@ defmodule RumblWeb.VideoChannel do
     handle_in(event, params, user, socket)
   end
 
-  def handle_in("new_annotation", params, user, socket) do
+def handle_in("new_annotation", params, user, socket) do
     case Multimedia.annotate_video(user, socket.assigns.video_id, params) do
       {:ok, annotation} ->
         broadcast!(socket, "new_annotation", %{
