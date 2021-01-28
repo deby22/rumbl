961d3c910f83e987d8e66e8b779d7d1c16098679
Dawid Dęby <dawid.deby@gmail.com>
2021-01-28 17:09:03 +0100

    Socket works on authenticated user. Generated also Annotation Model

--- lib/rumbl_web/channels/video_channel.ex
+++ lib/rumbl_web/channels/video_channel.ex
@@ -1,5 +1,6 @@
 defmodule RumblWeb.VideoChannel do
   use RumblWeb, :channel
+  
   def join("videos:" <> video_id, _params, socket) do
     # {:ok, assign(socket, :video_id, String.to_integer(video_id))}
     # :timer.send_interval(5_000, :ping)
--- lib/rumbl_web/controllers/auth.ex
+++ lib/rumbl_web/controllers/auth.ex
@@ -5,13 +5,6 @@ defmodule RumblWeb.Auth do
 
   def init(opts), do: opts
 
-  def login(conn, user) do
-    conn
-    |> put_current_user(user)
-    |> put_session(:user_id, user.id)
-    |> configure_session(renew: true)
-  end
-
   def logout(conn) do
     configure_session(conn, drop: true)
   end
@@ -31,6 +24,13 @@ defmodule RumblWeb.Auth do
       true ->
         assign(conn, :current_user, nil)
     end
+  end
+
+  def login(conn, user) do
+    conn
+    |> put_current_user(user)
+    |> put_session(:user_id, user.id)
+    |> configure_session(renew: true)
   end
 
   defp put_current_user(conn, user) do
