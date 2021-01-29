38491c46c59e68fc9ea30daea385f23e05b13bd7
Dawid Dęby <dawid.deby@gmail.com>
2021-01-29 16:57:44 +0100

    add new subapp to learn OTP

---
+++ info_sys/lib/info_sys/application.ex
@@ -0,0 +1,21 @@
+defmodule InfoSys.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      Supervisor.child_spec({InfoSys.Counter, 15}, id: :long),
+      Supervisor.child_spec({InfoSys.Counter, 5}, id: :short),
+      Supervisor.child_spec({InfoSys.Counter, 10}, id: :middle)
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: InfoSys.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
